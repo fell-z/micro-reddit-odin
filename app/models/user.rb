99075ec49dab67bb9a2ba6d4b185e0e9159ba1df
fell-z <matheusfelipeagapito@hotmail.com>
2024-08-15 20:16:13 -0300

    Add posts and associations to the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 5 }
