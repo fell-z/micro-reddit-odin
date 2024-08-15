bc2148331109f7747b2a249a4bc2553cc0f1d015
fell-z <matheusfelipeagapito@hotmail.com>
2024-08-15 19:27:06 -0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_15_220451) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_15_222345) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 end
